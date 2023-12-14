13df1e46b917f13528172e3f7604cb4a03995dfc
balkrishna <balkrishna.shrestha@itoffshorenepal.com>
2023-12-14 16:07:46 +0545

    added new php file

--- error.php
+++ error.php
@@ -2,3 +2,11 @@
 echo("<p>Search results for query: " .
     $_GET['query'] . ".</p>");
 ?>
+<?
+echo("<p>Search results for query: " .
+    $_GET['query'] . ".</p>");
+?>
+<?
+echo("<p>Search results for query: " .
+    $_GET['query'] . ".</p>");
+?>
