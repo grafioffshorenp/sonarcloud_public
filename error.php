6ecd04835697c44cfc74b67f0783db06d571b786
balkrishna <balkrishna.shrestha@itoffshorenepal.com>
2023-12-14 16:10:55 +0545

    updated php file

--- error.php
+++ error.php
@@ -1,12 +1,12 @@
-<?
+<?php
 echo("<p>Search results for query: " .
     $_GET['query'] . ".</p>");
 ?>
-<?
+<?php
 echo("<p>Search results for query: " .
     $_GET['query'] . ".</p>");
 ?>
-<?
+<?php
 echo("<p>Search results for query: " .
     $_GET['query'] . ".</p>");
 ?>
